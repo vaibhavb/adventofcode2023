7a5c870296326fe99809b329a70946ec6de660f1
Vaibhav Bhandari <vaibhavb@gmail.com>
2023-12-29 15:10:17 -0800

    solving part 4_2

--- 4_2.py
+++ 4_2.py
@@ -2,11 +2,10 @@ import sys
 import os
 from typing import List
 
+
 def get_card_values(cardlines):
-    card_values = []
-    card_values_total = 0
-    val_idx = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
-    for card in cardlines:
+    card_copies = [1 for _ in cardlines]
+    for idx, card in enumerate(cardlines):
         cardno = card.split(':')[0]
         cardnum = cardno.split(' ')[-1]
         rest = card.split(':')[1]
@@ -14,25 +13,27 @@ def get_card_values(cardlines):
         win_nums = winning_numbers.strip().split(' ')
         my_numbers = rest.strip().split('|')[1]
         my_nums = my_numbers.strip().split(' ')
-        values = []
+        values: list[int] = []
         for my_num in my_nums:
             if my_num != '':
                 if my_num in win_nums:
-                    values.append(my_num)
-        # print(f"values: {values}, total: {len(values)}")
-        card_values.append(val_idx[len(values)])
-        card_values_total += val_idx[len(values)]
-    for idx, card_value in enumerate(card_values):
+                    # update the number of insert cards as copies
+                    values.append(int(my_num))
+        for _ in range(0, card_copies[idx]):
+            for copy_idx, _ in enumerate(values):
+                card_copies[idx + copy_idx + 1] += 1
+        print(f"{idx}: {len(values)}")
+    for idx, card_value in enumerate(card_copies):
         print(f"{idx}: {card_value}")
-    return card_values_total
+    total = sum(card_copies)
+    print(f"The total : {total}")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) == 2:
         filename = sys.argv[1]
     else:
         filename = (os.path.basename(__file__)
                     .replace("py", "in")
-                    .replace("_1", ""))
+                    .replace("_2", ""))
     cardlines: list[str] = open(filename).read().strip().split('\n')
-    print(get_card_values(cardlines))
+    get_card_values(cardlines)
